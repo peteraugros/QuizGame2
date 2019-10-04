fb9adb236ae0750fef67c1b420946b3ca896934b
bevincass <peteraugros@gmail.com>
2019-10-04 11:24:20 -0600

    populated question upon pushing begin game button

--- assets/quizGame.js
+++ assets/quizGame.js
@@ -1,3 +1,38 @@
+//setting variables
+
+
+//array of questions
+var questions = [
+
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<scripting>", "<javascript>", "<js>"],
+        answer: "<script>"
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["The <body> section", "The <head> or <body>", "the <head> section", "In the <style> tags"],
+        answer: "The <head> or <body>"
+    },
+    {
+        title: "How do you write 'Hello World' in an alert box?",
+        choices: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
+        answer: "alert('Hello World);"
+    },
+    {
+        title: "How do you call a function in JavaScript?",
+        choices: ["call myFunction", "call function myFunction", "myFunction()", "myFunction"],
+        answer: "myFunction()"
+    }
+];
+
+//making buttons visible and setting up question in text area and setting up questions on buttons
 function setUp(){
+    //making buttons visible
     document.getElementById("btn2").style.visibility = "visible";
+    document.getElementById("btn3").style.visibility = "visible";
+    document.getElementById("btn4").style.visibility = "visible";
+
+    //setting up question
+    document.getElementById("questionSpace").textContent = questions[0].title;
 }
